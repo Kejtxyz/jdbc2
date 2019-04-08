59810281391705f564166222b47bc8d1dc1918ad
kasia <kgbsz@wp.pl>
2019-04-08 18:29:59 +0200

    bazy danych  - polaczenie - jdbc - sql

--- src/main/java/jdbcExample1/JdbcMain.java
+++ src/main/java/jdbcExample1/JdbcMain.java
@@ -3,16 +3,21 @@ package jdbcExample1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 // wczesniej wlaczyc xampp
 public class JdbcMain {
 // serverTimezone- wymusza zgodnosc z baza danych pomiedzy strefami czasowymi
     public static final String URL = "jdbc:mysql://localhost:3306/tarr4_db?serverTimezone=UTC";     // adres do tabeli w bazie danych  // ctrl+alt+c
     public static final String USER = "klient";     // nzwa user - dostep do bazy danych sql
     public static final String PASSWORD = "123";   // hasło do bazy danych sql
+    public static final String SQL = "SQL";
 
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();  // var. - wyciaga zmienna,generuje
+            statement.executeQuery(SQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
