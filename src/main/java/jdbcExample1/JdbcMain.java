d9146a1e7c33735e897e63c49be1b82215a685aa
kasia <kgbsz@wp.pl>
2019-04-08 19:09:36 +0200

    bazy danych  - polaczenie - jdbc - sql zmiana z trenerow na tablice zawodnikow z workbeach

--- src/main/java/jdbcExample1/JdbcMain.java
+++ src/main/java/jdbcExample1/JdbcMain.java
@@ -8,18 +8,26 @@ public class JdbcMain {
     public static final String URL = "jdbc:mysql://localhost:3306/tarr4_db?serverTimezone=UTC";     // adres do tabeli w bazie danych  // ctrl+alt+c
     public static final String USER = "klient";     // nzwa user - dostep do bazy danych sql
     public static final String PASSWORD = "123";   // hasło do bazy danych sql
-    public static final String SQL = "select * from trenerzy";
-
+    public static final String SQL = "select * from zawodnicy where nazwisko = 'MALYSZ'";
+// prepare statment - jest zabezpieczony na ataki sql injection-wstrzykiwanie sql
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            Statement statement = connection.createStatement();  // var. - wyciaga zmienna,generuje
-            ResultSet resultSet = statement.executeQuery(SQL);  // executequery - do bazy danych wysle zapytanie sql, do bazy sql / executeUpdate- inser,update,delete-zrob cos w bazie danych w rekordzie
+            Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_UPDATABLE);
+            DatabaseMetaData metaData = connection.getMetaData();
+            if (metaData.supportsResultSetConcurrency(ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_UPDATABLE))
+                System.out.println("kotek");
+
+            //CONCUR_READ_ONLY,ResultSet.CONCUR_UPDATABLE - pytamy czy jest tego typu
+            // _FORWARD_ONLY,ResultSet.CONCUR_UPDATABLE - ustawienie defualt, jak bedziemy przegladac wyniki to bedziemy mogli je przesuwac tlyko w jednoa storne do przodu,/ ca - oprucz przeczytania wynikow z sql, bedziemy mogli etez zmieniac dane w bazie danych/ // var. - wyciaga zmienna,generuje
+            ResultSet resultSet = statement.executeQuery(SQL);         // executequery - do bazy danych wysle zapytanie sql, do bazy sql / executeUpdate- inser,update,delete-zrob cos w bazie danych w rekordzie
             while (resultSet.next()){
-                String imie_t = resultSet.getString("imie_t");
-                String nazwisko_t = resultSet.getString("nazwisko_t");
-                int l_zawodnikow = resultSet.getInt("l_zawodnikow");
-                System.out.println(imie_t + " " +nazwisko_t +" " + l_zawodnikow);
+                String imie = resultSet.getString("imie");
+                String nazwisko = resultSet.getString("nazwisko");
+                int wzrost = resultSet.getInt("wzrost");
+                System.out.println(imie + " " + nazwisko +" " + wzrost);
+                resultSet.updateString("imie","Marcin");
+                System.out.println(imie + " " + nazwisko +" " + wzrost);
             }
         } catch (SQLException e) {
             e.printStackTrace();
